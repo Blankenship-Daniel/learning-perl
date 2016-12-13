12fa54b6e8ea4a674efd0705a2450068c9234e0b
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-12-13 09:38:14 -0700

    added loops examples

--- objects/Person.pm
+++ objects/Person.pm
@@ -1,36 +1,49 @@
-#!/usr/bin/perl
+package Person;
+
 use strict;
 use warnings;
 
-package Person;
-
 sub new {
     my $class = shift;
-    my $self = {
-        _first_name => shift,
-        _last_name  => shift,
-        _ssn        => shift,
-    };
-    bless $self, $class;
-    return $self;
+    return bless shift, $class;
 }
 
 sub get_first_name {
-    print "get_first_name self: $self\n";
-    print "get_first_name self->first_name: $self->{_first_name}\n";
-
-    return $self->{_first_name};
+    my ($self) = @_;
+    return $self->{_first_name} || "";
 }
 
 sub set_first_name {
     my ($self, $first_name) = @_;
-
-    print "set_first_name self: $self\n";
-    print "set_first_name first_name: $first_name\n";
-    
     $self->{_first_name} = $first_name if defined($first_name);
-    
     return $self->{_first_name};
+}
+
+sub get_last_name {
+    my ($self) = @_;
+    return $self->{_last_name} || "";
+}
+
+sub set_last_name {
+    my ($self, $last_name) = @_;
+    $self->{_last_name} = $last_name if defined($last_name);
+    return $self->{_last_name};
+}
+
+sub get_ssn {
+    my ($self) = @_;
+    return $self->{_ssn} || -1;
+}
+
+sub set_ssn {
+    my ($self, $ssn) = @_;
+    $self->{_ssn} = $ssn if defined($ssn);
+    return $self->{_ssn};
+}
+
+sub to_string {
+    my ($self) = @_;
+    return "$self->{_first_name}\n$self->{_last_name}\n$self->{_ssn}\n";
 }
 
 1;
--- objects/person_driver.pl
+++ objects/person_driver.pl
@@ -4,6 +4,24 @@ use warnings;
 
 use Person;
 
-$person = new Person("Daniel", "Blankenship", 111111111);
+my $person = new Person({
+    _first_name => "Daniel",
+    _last_name  => "Blankenship",
+    _ssn        => 111111111
+});
+
 $person->set_first_name("Dan");
-$person->get_first_name();
+my $first_name = $person->get_first_name();
+
+$person->set_last_name("Blank");
+my $last_name = $person->get_last_name();
+
+$person->set_ssn(1234);
+my $ssn = $person->get_ssn();
+
+print "My first name is $first_name\n";
+print "My last name is $last_name\n";
+print "My ssn is $ssn\n\n";
+
+print "Testing to_string method: \n";
+print $person->to_string();
